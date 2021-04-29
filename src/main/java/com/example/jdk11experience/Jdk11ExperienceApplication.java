bdfa0219f01ed96f3427451181040b3625211402
Chauncey <sunchangxin@carcredit.com.cn>
2021-04-29 18:57:54 +0800

    这个测试项目尝试了一份代码分别提交到了Gitee和GitHub上面

--- src/main/java/com/example/jdk11experience/Jdk11ExperienceApplication.java
+++ src/main/java/com/example/jdk11experience/Jdk11ExperienceApplication.java
@@ -3,6 +3,13 @@ package com.example.jdk11experience;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 这个测试项目尝试了一份代码分别提交到了Gitee和GitHub上面
+ * 这是我第一次这样尝试
+ *
+ * @author Chauncey Sun
+ * @create 2021/4/29 18:56
+ */
 @SpringBootApplication
 public class Jdk11ExperienceApplication {
 
