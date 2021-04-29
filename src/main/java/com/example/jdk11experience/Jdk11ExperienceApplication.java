1e995c84eecd25beea45e9743c9b3ee58bca598f
Chauncey <sunchangxin@carcredit.com.cn>
2021-04-29 11:36:32 +0800

    Initial commit

---
+++ src/main/java/com/example/jdk11experience/Jdk11ExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdk11experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk11ExperienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk11ExperienceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdk11experience/Jdk11ExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdk11experience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk11ExperienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
